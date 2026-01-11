27ff924080660d6eafbe47a156725ced56ed2a76
Intekhab Ansari <ansariintekhab47@gmail.com>
2026-01-11 17:00:53 +0530

    fetching api and defining datatype was done

---
+++ GrowMeOrganic/src/hooks/useArtWork.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import type { Artwork } from "../type/ArtWork";
+
+const page_size = 12;
+
+export function useArtWorks(page:number){
+    const [artworks, setArtworks] = useState<Artwork[]>([]);
+    const [total, setTotal] = useState(0);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        setLoading(true);
+
+        fetch(`https://api.artic.edu/api/v1/artworks?page=${page+1}&limit=${page_size}`)
+            .then(res => res.json())
+            .then(result =>{
+                setArtworks(result.data)
+                setTotal(result.pagination.total);
+            })
+            .finally(()=>setLoading(false));
+    },[page]);
+    return{artworks,total,loading,page_size};
+}
+
+// seperat api fetch so code get clean and easy to understand
---
+++ GrowMeOrganic/src/type/artWork.ts
@@ -0,0 +1,11 @@
+export interface Artwork {
+    id:number;
+    title:string;
+    place_of_origin:string;
+    artist_display: string;
+    incriptions:string;
+    date_start:number;
+    date_end:number;
+}
+
+// defining data type for future use
