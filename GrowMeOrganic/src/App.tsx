b822a770b6934deb4eab2d44d5aa679a6750753a
Intekhab Ansari <ansariintekhab47@gmail.com>
2026-01-11 22:43:38 +0530

    almost working but not better

--- GrowMeOrganic/src/App.tsx
+++ GrowMeOrganic/src/App.tsx
@@ -1,8 +1,29 @@
-// import React from 'react'
+import { useState } from "react"
+import { useArtWorks } from "./hooks/useArtWork";
+import { ArtWorkTable } from "./components/ArtWorkTable";
 
 const App = () => {
+
+  const [page, setPage] = useState(0);
+  const [selectedIds, setSelectedIds] = useState<Set<number>>(new Set());
+
+  const{artworks, total, loading, page_size} = useArtWorks(page);
+
   return (
-    <div>Hello</div>
+    <div style={{padding:24}}>
+      <h2>Art Institute Artwork List</h2>
+
+      <ArtWorkTable
+        data={artworks}
+        total={total}
+        page={page}
+        rows={page_size}
+        loading={loading}
+        selectedIds={selectedIds}
+        onPageChange={setPage}
+        onSelectionUpdate={setSelectedIds}
+      />
+    </div>
   )
 }
 
--- GrowMeOrganic/src/components/ArtWorkTable.tsx
+++ GrowMeOrganic/src/components/ArtWorkTable.tsx
@@ -1,12 +1,57 @@
-// import React from 'react'
+import type { Artwork } from "../type/ArtWork";
+import {DataTable, type DataTableSelectionMultipleChangeEvent} from "primereact/datatable";
+import { Column } from "primereact/column";
 
-const ArtWorkTable = () => {
-
-  
-
-  return (
-    <div>ArtWorkTable</div>
-  )
+interface Props{
+    data:Artwork[];
+    total:number;
+    page:number;
+    rows:number;
+    loading:boolean;
+    selectedIds:Set<number>;
+    onPageChange:(page:number)=>void;
+    onSelectionUpdate:(ids:Set<number>) => void;
 }
 
-export default ArtWorkTable
+export function ArtWorkTable({
+  data,
+  total,
+  page,
+  rows,
+  loading,
+  selectedIds,
+  onPageChange,
+  onSelectionUpdate,
+}: Props){
+  const selectedRows = data.filter(item => selectedIds.has(item.id));
+
+  const handleSelectionChange=(e:DataTableSelectionMultipleChangeEvent<Artwork[]>)=>{
+    
+    onSelectionUpdate(new Set(e.value.map(row =>row.id )));
+  };
+
+  return(
+    <DataTable
+      value={data}
+      paginator
+      lazy
+      loading={loading}
+      rows={rows}
+      totalRecords={total}
+      first={page*rows}
+      onPage={(e)=>onPageChange(e.page ?? 0)}
+      dataKey="id"
+      selectionMode="multiple"
+      selection={selectedRows}
+      onSelectionChange={handleSelectionChange}
+    >
+      <Column selectionMode="multiple" headerStyle={{width:"3rem "}}/>
+      <Column field="title" header="title" />
+      <Column field="place_of_origin" header="Origin"/>
+      <Column field="artist_display" header="Artist"/>
+      <Column field="incriptions" header="Incriptions"/>
+      <Column field="date_start" header="Start Year" />
+      <Column field="date_end" header="End Year"/>
+    </DataTable>
+  );
+}
